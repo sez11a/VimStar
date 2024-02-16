9889a0a31fc3486d514c52dbdb9a4b739b8a687e
Rich Sezov <richard.sezov@liferay.com>
2024-02-16 16:06:28 -0500

    Add user config to install.

--- install-vimstar.sh
+++ install-vimstar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "This script installs VimStar, a Vim/NeoVim distribution with a writing and programming focus."
+echo "This script installs VimStar, a Neovim distribution with a writing and programming focus."
 echo "Beginning install..."
 
 cd ~
@@ -19,20 +19,20 @@ mkdir -p ~/.config/
 # Ensure spell exists
 mkdir -p ~/.VimStar/spell
 
-echo "Symbolic linking Vim/NeoVim config..."
-ln -sfnv ~/.VimStar ~/.vim
+echo "Symbolic linking Neovim config..."
+#ln -sfnv ~/.VimStar ~/.vim
 ln -sfnv ~/.VimStar ~/.config/nvim
-ln -sfnv ~/.VimStar/init.vim ~/.VimStar/vimrc
-ln -sfnv ~/.VimStar/options/gui.vim ~/.VimStar/ginit.vim
+#ln -sfnv ~/.VimStar/init.vim ~/.VimStar/vimrc
+#ln -sfnv ~/.VimStar/options/gui.vim ~/.VimStar/ginit.vim
 
 if [ ! -f ".VimStar.vimrc" ]; then
     echo "Copying default config to ~/.VimStar.vimrc"
-    cp ~/.VimStar/VimStar-user.vimrc ~/.VimStar-user.vimrc
+    cp ~/.VimStar/vimstar-user-template ~/.VimStar/vimstar-user.lua
 fi
 
 # echo "Installing Vim-Plug"
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Install finished! Run Vim/NeoVim and execute :PlugInstall"
+echo "Install finished! Run Neovim to finish setup!"
 
