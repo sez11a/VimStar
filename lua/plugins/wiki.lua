6dba5ec04ab98f0b36c667478f65d0988d6c2381
Rich Sezov <richard.sezov@liferay.com>
2024-04-23 16:48:20 -0400

    Configure wiki journal format

--- lua/plugins/wiki.lua
+++ lua/plugins/wiki.lua
@@ -1,4 +1,20 @@
-return { 
+return {
+  {
   'lervag/wiki.vim',
+    config = function()
+      vim.g.wiki_journal = {
+        name = 'journal',
+        root = '',
+        frequency = 'daily',
+        date_format = {
+          daily = '%Y/%m/%d',
+          weekly = '%Y/week_%V',
+          monthly = '%Y/%m/summary',
+        },
+      }
+    end
+  },
+  {
   'lervag/lists.vim',
+  }
 }
