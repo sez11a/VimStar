052f7bf5dcb897ff292d1c62a21765b2c8cb7d0d
Rich Sezov <richard.sezov@liferay.com>
2024-05-01 16:06:35 -0400

    Finalize install. Enough to make master; still porting functionality

--- install-vimstar.sh
+++ install-vimstar.sh
@@ -20,19 +20,12 @@ mkdir -p ~/.config/
 mkdir -p ~/.VimStar/spell
 
 echo "Symbolic linking Neovim config..."
-#ln -sfnv ~/.VimStar ~/.vim
 ln -sfnv ~/.VimStar ~/.config/nvim
-#ln -sfnv ~/.VimStar/init.vim ~/.VimStar/vimrc
-#ln -sfnv ~/.VimStar/options/gui.vim ~/.VimStar/ginit.vim
 
 if [ ! -f ".VimStar.vimrc" ]; then
     echo "Copying default config to ~/.VimStar.vimrc"
     cp ~/.VimStar/vimstar-user-template ~/.VimStar/lua/vimstar-user.lua
 fi
-
-# echo "Installing Vim-Plug"
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Install finished! Run Neovim to finish setup!"
 
