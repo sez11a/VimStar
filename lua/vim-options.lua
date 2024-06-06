5396eb4e3f4e802007167529bddbfcc17bf36af0
Rich Sezov <richard.sezov@liferay.com>
2024-06-06 13:22:45 -0400

    For Liferay: get rid of eol at eof

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.cmd("filetype plugin indent on")
 vim.cmd("set guicursor+=i:blinkon100")
+vim.cmd("set nofixeol")
 vim.opt.encoding = "utf-8"
 vim.opt.signcolumn = "yes"
 vim.opt.syntax = "on"
