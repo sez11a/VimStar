603e3bf66bc6af58b3671752c3f5634fa3de88a7
Rich Sezov <richard.sezov@liferay.com>
2024-09-12 15:19:52 -0400

    Update tsserver to ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,7 +20,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 --      require('java').setup()
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.html.setup({
