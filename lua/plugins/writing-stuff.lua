01fbe1e9d454b58dcd93fd123dbafc948a9b39d4
Rich Sezov <richard.sezov@liferay.com>
2024-09-27 18:31:38 -0400

    Replace VOOM with Outline.

--- lua/plugins/writing-stuff.lua
+++ lua/plugins/writing-stuff.lua
@@ -51,6 +51,25 @@ return {
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
+--  {
+--    "3rd/image.nvim",
+--    config = function()
+--        require('image').setup({
+--        integrations = {
+--          markdown = {
+--            resolve_image_path = function(document_path, image_path, fallback)
+--              -- document_path is the path to the file that contains the image
+--              -- image_path is the potentially relative path to the image. for
+--              -- markdown it's `![](this text)`
+--
+--              -- you can call the fallback function to get the default behavior
+--              return fallback(document_path, image_path)
+--            end,
+--          }
+--        }
+--      })
+--    end
+-- },
   {
     "FotiadisM/tabset.nvim",
       config = function()
