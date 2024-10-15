0634bb84998d9f280f07839f9452a0fe3ee0e769
Rich Sezov <richard.sezov@liferay.com>
2024-10-15 16:41:45 -0400

    Lualine tweaking

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,42 @@ return {
     priority = 1000,
     config = function()
       vim.cmd.colorscheme "catppuccin-macchiato"
+      require("catppuccin").setup({
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          treesitter = true,
+          alpha = true,
+          dap = true,
+          dap_ui = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+                errors = { "italic" },
+                hints = { "italic" },
+                warnings = { "italic" },
+                information = { "italic" },
+                ok = { "italic" },
+            },
+            underlines = {
+                errors = { "underline" },
+                hints = { "underline" },
+                warnings = { "underline" },
+                information = { "underline" },
+                ok = { "underline" },
+            },
+            inlay_hints = {
+                background = true,
+            },
+          },
+          telescope = {
+            enabled = true,
+          },
+          gitgutter = true,
+          which_key = true,
+        }
+      })
     end
   }
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,8 +6,12 @@ return {
           options = {
             icons_enabled = true,
             theme = "auto",
-            component_separators = { left = "|", right = "|" },
-            section_separators = { left = "|", right = "|" },
+            -- component_separators = { left = "|", right = "|" },
+            -- section_separators = { left = "|", right = "|" },
+            -- section_separators = { left = '', right = '' },
+            -- component_separators = { left = '', right = '' },
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
           },
           sections = {
             lualine_a = { "mode" },
