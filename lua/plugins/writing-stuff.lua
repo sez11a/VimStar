f07c1ea2c933eb6774fd3f0bef460c119ee004b8
Rich Sezov <richard.sezov@liferay.com>
2024-12-06 19:48:20 -0500

    Add Marp; add first Pandoc template.

--- lua/plugins/writing-stuff.lua
+++ lua/plugins/writing-stuff.lua
@@ -113,4 +113,14 @@ return {
       })
       end
   },
+  {
+    "mpas/marp-nvim",
+      config = function()
+        require("marp").setup({
+          port = 8084,
+          wait_for_response_timeout = 30,
+          wait_for_response_delay = 1,
+        })
+      end,
+  },
 }
