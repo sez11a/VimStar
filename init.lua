11e68c245be48eae4a07aea38f67abea22ae82b7
Rich Sezov <richard.sezov@liferay.com>
2025-04-07 17:36:00 -0400

    Fix Neovide transparency

--- init.lua
+++ init.lua
@@ -20,7 +20,10 @@ require("lazy").setup("plugins")
 require("keymaps")
 require("vimstar-user")
 
+vim.g.transparency = 0.8
+
 if vim.g.neovide then 
-  vim.g.neovide_transparency = 0.4
+ -- vim.g.neovide_opacity = 0.0
+  vim.g.neovide_normal_opacity = 0.8
   vim.g.neovide_window_blurred = true
 end
